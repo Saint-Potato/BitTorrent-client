649e5a7aa1214ef9c4bb59a642742ae2937c5993
Aryan <aryanthapliyal@yahoo.co.in>
2025-03-13 14:12:02 +0530

    decode bencoded list

--- app/main.py
+++ app/main.py
@@ -8,22 +8,65 @@ import sys
 #
 # - decode_bencode(b"5:hello") -> b"hello"
 # - decode_bencode(b"10:hello12345") -> b"hello12345"
+
 def decode_bencode(bencoded_value):
     #decode strings
-    if chr(bencoded_value[0]).isdigit():
-        first_colon_index = bencoded_value.find(b":")
-        if first_colon_index == -1:
-            raise ValueError("Invalid encoded value")
-        return bencoded_value[first_colon_index+1:]
-    #decode integers
-    elif chr(bencoded_value[0]) == 'i':
-        integer_start_index = bencoded_value[1]
-        end_index = bencoded_value.find(b"e")
-        if end_index == -1:
-            raise ValueError("Invalid encoded value")
-        return int(bencoded_value[1:end_index])
-    else:
-        raise NotImplementedError("Only strings are supported at the moment")
+    def extract_string(data):
+        length, rest = data.split(b":", 1)
+        length = int(length)
+        return rest[:length], rest[length:]
+    def decode(data):
+        if chr(data[0]).isdigit():
+            decoded_str, rest = extract_string(data)
+            return decoded_str, rest
+        elif chr(data[0]) == 'i':
+            end = data.find(b"e")
+            return int(data[1:end]), data[end+1:]
+        elif chr(data[0]) == 'l':
+            data = data[1:]
+            list_decoded = []
+            while not data.startswith(b"e"):
+                item, data = decode(data)
+                list_decoded.append(item)
+            return list_decoded, data[1:]
+        else:
+            raise NotImplementedError("Only strings are supported at the moment")
+    decoded_value, _ = decode(bencoded_value)
+    return decoded_value
+    # if chr(bencoded_value[0]).isdigit():
+    #     first_colon_index = bencoded_value.find(b":")
+    #     if first_colon_index == -1:
+    #         raise ValueError("Invalid encoded value")
+    #     return bencoded_value[first_colon_index+1:]
+    # #decode integers
+    # elif chr(bencoded_value[0]) == 'i':
+    #     integer_start_index = bencoded_value[1]
+    #     end_index = bencoded_value.find(b"e")
+    #     if end_index == -1:
+    #         raise ValueError("Invalid encoded value")
+    #     return int(bencoded_value[1:end_index])
+    # #decode lists
+    # elif chr(bencoded_value[0]) == 'l':
+    #     decoded_list = []
+    #     item_start = None
+    #     item_end = None
+    #     i = 1
+    #     while(i < len(bencoded_value)) and bencoded_value[i] != ord(b"e"):
+    #         if(chr(bencoded_value[i]) == 'i'):
+    #             item_end = bencoded_value.find(b"e", i)
+    #             item = decode_bencode(bencoded_value[i:item_end+1])
+    #             i = item_end + 1
+    #             decoded_list.append(item)
+    #         else:
+    #             colon_index = bencoded_value.find(b":", i)
+    #             string_length = int((bencoded_value[i:colon_index].decode()))
+    #             item = decode_bencode(bencoded_value[i:colon_index + string_length])
+    #             decoded_list.append(item)
+    #             i += colon_index + string_length + 1
+    #     return decoded_list
+
+    # else:
+    #     raise NotImplementedError("Only strings are supported at the moment")
 
 
 def main():
