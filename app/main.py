ea144a09e30815b1f02d1f877898e6ec37d3e358
Aryan <aryanthapliyal@yahoo.co.in>
2025-03-14 14:56:35 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,72 +11,60 @@ import bencodepy
 
 def decode_bencode(bencoded_value):
     #decode strings
-    bc = bencodepy.Bencode(encoding = "utf-8")
-    return bc.decode(bencoded_value)
-    def extract_string(data):
-        length, rest = data.split(b":", 1)
-        length = int(length)
-        return rest[:length], rest[length:]
-    def decode(data):
-        if chr(data[0]).isdigit():
-            decoded_str, rest = extract_string(data)
-            return decoded_str, rest
-        elif chr(data[0]) == 'i':
-            end = data.find(b"e")
-            return int(data[1:end]), data[end+1:]
-        elif chr(data[0]) == 'l':
-            data = data[1:]
-            list_decoded = []
-            while not data.startswith(b"e"):
-                item, data = decode(data)
-                list_decoded.append(item)
-            return list_decoded, data[1:]
-        elif chr(data[0] == 'd'):
-            data = b"l" + data[1:]
-            res_list = decode_bencode(data)
-            decoded_dictionary = {}
-            for i in range(len(res_list)):
-                decoded_dictionary[res_list[i]] = res_list[i+1]
-                i+=1
-            return decoded_dictionary
-        else:
-            raise NotImplementedError("Only strings are supported at the moment")
-    decoded_value, _ = decode(bencoded_value)
-    return decoded_value
-    # if chr(bencoded_value[0]).isdigit():
-    #     first_colon_index = bencoded_value.find(b":")
-    #     if first_colon_index == -1:
-    #         raise ValueError("Invalid encoded value")
-    #     return bencoded_value[first_colon_index+1:]
-    # #decode integers
-    # elif chr(bencoded_value[0]) == 'i':
-    #     integer_start_index = bencoded_value[1]
-    #     end_index = bencoded_value.find(b"e")
-    #     if end_index == -1:
-    #         raise ValueError("Invalid encoded value")
-    #     return int(bencoded_value[1:end_index])
-    # #decode lists
-    # elif chr(bencoded_value[0]) == 'l':
-    #     decoded_list = []
-    #     item_start = None
-    #     item_end = None
-    #     i = 1
-    #     while(i < len(bencoded_value)) and bencoded_value[i] != ord(b"e"):
-    #         if(chr(bencoded_value[i]) == 'i'):
-    #             item_end = bencoded_value.find(b"e", i)
-    #             item = decode_bencode(bencoded_value[i:item_end+1])
-    #             i = item_end + 1
-    #             decoded_list.append(item)
-    #         else:
-    #             colon_index = bencoded_value.find(b":", i)
-    #             string_length = int((bencoded_value[i:colon_index].decode()))
-    #             item = decode_bencode(bencoded_value[i:colon_index + string_length])
-    #             decoded_list.append(item)
-    #             i += colon_index + string_length + 1
-    #     return decoded_list
+    # bc = bencodepy.Bencode(encoding = "utf-8")
+    # return bc.decode(bencoded_value)
+    decoded = bencodepy.decode(bencoded_value)
+    def bytes_to_str(data):
+        if isinstance(data, dict):
+            return {k.decode(): bytes_to_str(v) for k, v in data.items()}  # Convert keys
+        elif isinstance(data, list):
+            return [bytes_to_str(i) for i in data]  # Convert list elements
+        elif isinstance(data, bytes):
+            return data.decode(errors="ignore")  # Convert bytes to string safely
+        return data  # Leave other data types unchanged
 
-    # else:
-    #     raise NotImplementedError("Only strings are supported at the moment")
+    return bytes_to_str(decoded)
+
+    # def extract_string(data):
+    #     length, rest = data.split(b":", 1)
+    #     length = int(length)
+    #     return rest[:length], rest[length:]
+    # def decode(data):
+    #     if chr(data[0]).isdigit():
+    #         decoded_str, rest = extract_string(data)
+    #         return decoded_str, rest
+    #     elif chr(data[0]) == 'i':
+    #         end = data.find(b"e")
+    #         return int(data[1:end]), data[end+1:]
+    #     elif chr(data[0]) == 'l':
+    #         data = data[1:]
+    #         list_decoded = []
+    #         while not data.startswith(b"e"):
+    #             item, data = decode(data)
+    #             list_decoded.append(item)
+    #         return list_decoded, data[1:]
+    #     elif chr(data[0] == 'd'):
+    #         data = b"l" + data[1:]
+    #         res_list = decode_bencode(data)
+    #         decoded_dictionary = {}
+    #         for i in range(len(res_list)):
+    #             decoded_dictionary[res_list[i]] = res_list[i+1]
+    #             i+=1
+    #         return decoded_dictionary
+    #     else:
+    #         raise NotImplementedError("Only strings are supported at the moment")
+    # decoded_value, _ = decode(bencoded_value)
+    # return decoded_value
+
+def parse_torrent(torrent_path):
+    f = open(torrent_path, "rb")
+    meta_info = f.read()
+    result = decode_bencode(meta_info)
+    tracker_url = result['announce']
+    file_size = result['info']['length']
+    return tracker_url,file_size
+
+
 
 
 def main():
@@ -100,6 +88,13 @@ def main():
 
         # Uncomment this block to pass the first stage
         print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+
+    elif command == "info":
+        torrent_path = sys.argv[2]
+        tracker_url, file_size = parse_torrent(torrent_path)
+        print(f"Tracker URL: {tracker_url}")
+        print(f"Length: {file_size}" )
+
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
--- app/rough.py
+++ app/rough.py
@@ -1,3 +1,14 @@
-text = "hello"
-byte_text = text.encode()  # Default encoding is UTF-8
-print(byte_text)  # b'hello'
+import bencodepy
+
+def decode_bencode(bencoded_value):
+    return bencodepy.decode(bencoded_value)
+ 
+
+file_path = "sample.torrent"
+f = open(file_path, "rb")
+meta_info = f.read()
+result = decode_bencode(meta_info)
+tracker_url = result[b'announce'].decode()
+file_size = result[b'info'][b'length']
+print("Tracker URL: ",tracker_url)
+print("Length: ", file_size)
